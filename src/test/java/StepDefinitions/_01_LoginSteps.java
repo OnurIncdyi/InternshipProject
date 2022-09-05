98739443aa8e002a0d035c54e21d7dfed2ab560c
OnurIncdyi <108151328+OnurIncdyi@users.noreply.github.com>
2022-09-05 18:09:08 +0300

    Create PG16-8 and Delete

--- src/test/java/StepDefinitions/_01_LoginSteps.java
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -2,9 +2,11 @@ package StepDefinitions;
 
 import Pages.DialogContent;
 import Utilities.GWD;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -47,5 +49,14 @@ public class _01_LoginSteps {
 
         dc.findAndContainsText("dashboard","Dashboard");
         dc.findAndClick("acceptCookies");
+    }
+
+    @And("wait")
+    public void invisibleLoading() {
+        try {
+            Thread.sleep(  5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
