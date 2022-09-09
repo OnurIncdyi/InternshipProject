7439e64564d7c5364cf6029598110767c849baf7
asus <makif_cetin@hotmail.com>
2022-09-10 01:21:58 +0300

    PG16-3 TestRunner

---
+++ src/test/java/Runners/PG16_3_TestRunner.java
@@ -0,0 +1,14 @@
+package Runners;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+
+        tags = "@Regression",
+        features = "src/test/java/FeatureFiles/PG16_3.feature",
+        glue = "StepDefinitions",
+        plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
+
+)
+public class PG16_3_TestRunner extends AbstractTestNGCucumberTests {
+}
